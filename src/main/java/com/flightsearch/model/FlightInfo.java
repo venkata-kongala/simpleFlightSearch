28889b0b3fec2e2db06a4719dc2b487211283b3f
venkata <venkata.kongala@gmail.com>
2019-05-05 16:05:49 +0530

    My commit.

--- src/main/java/com/flightsearch/model/FlightInfo.java
+++ src/main/java/com/flightsearch/model/FlightInfo.java
@@ -1,15 +1,3 @@
-/*
- * InstaReM Overseas Money Transfer.
- * https://www.instarem.com/en-in/
- *
- * Copyright (c) 2019 InstaReM
- *
- * This file is licensed and cannot be accessed outside InstaReM.
- * It can only be accessed and modified by the authorized InstaReM Technical Teams.
- * All unauthorized modifications to the content of this file,
- * will be prosecuted under the Copyright Act.
- *
- */
 package com.flightsearch.model;
 
 import lombok.Builder;
